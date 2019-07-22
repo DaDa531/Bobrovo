9cf93da1943f45487cbebc9638dcf71bfbba15ac
DaDa531 <dan.bezakova@gmail.com>
2019-07-22 23:31:56 +0200

    Task - create form

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -17,9 +17,37 @@
                     <label for="question">Zadanie</label>
                     <textarea id="question" class="form-control wyswyg-editor" name="description" rows=3" ></textarea>
 
-                    Otázka
+                    <label for="answer-a">Odpoveď a)</label>
+                    <input id="answer-a" type="text" class="mb-2 form-control{{ $errors->has('answer-a') ? ' is-invalid' : '' }}" name="answer-a" value="{{ old('answer-a') }}" required autofocus>
+                    @if ($errors->has('answer-a'))
+                        <span class="invalid-feedback">
+                                <strong>{{ $errors->first('answer-a') }}</strong>
+                            </span>
+                    @endif
 
-                    Možnosti - 4x obyč input
+                    <label for="answer-b">Odpoveď b)</label>
+                    <input id="answer-b" type="text" class="mb-2 form-control{{ $errors->has('answer-b') ? ' is-invalid' : '' }}" name="answer-b" value="{{ old('answer-b') }}" required autofocus>
+                    @if ($errors->has('answer-b'))
+                        <span class="invalid-feedback">
+                                <strong>{{ $errors->first('answer-b') }}</strong>
+                            </span>
+                    @endif
+
+                    <label for="answer-c">Odpoveď c)</label>
+                    <input id="answer-c" type="text" class="mb-2 form-control{{ $errors->has('answer-c') ? ' is-invalid' : '' }}" name="answer-c" value="{{ old('answer-c') }}" required autofocus>
+                    @if ($errors->has('answer-c'))
+                        <span class="invalid-feedback">
+                                <strong>{{ $errors->first('answer-c') }}</strong>
+                            </span>
+                    @endif
+
+                    <label for="answer-d">Odpoveď d)</label>
+                    <input id="answer-d" type="text" class="mb-2 form-control{{ $errors->has('answer-d') ? ' is-invalid' : '' }}" name="answer-d" value="{{ old('answer-d') }}" required autofocus>
+                    @if ($errors->has('answer-d'))
+                        <span class="invalid-feedback">
+                                <strong>{{ $errors->first('answer-d') }}</strong>
+                            </span>
+                    @endif
 
                     Správna odpoveď - výber zo 4
 
